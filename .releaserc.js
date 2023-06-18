f697b5b9da9a68bb000a8d6696bb3e0c26097ec0
Elvis Pereira <elvispereira@hotmail.com>
2023-06-18 00:59:13 +0000

    feat: update module and translations to be compatible with foundry v11
    - Translation strings have been updated to latest Foundry release v11.301.

--- .releaserc.js
+++ .releaserc.js
@@ -1,55 +1,49 @@
 module.exports = {
   branches: [
-    'master',
+    "master",
     {
-      name: 'beta',
-      prerelease: true
-    }
+      name: "beta",
+      prerelease: true,
+    },
   ],
   plugins: [
     [
-      '@semantic-release/commit-analyzer',
+      "@semantic-release/commit-analyzer",
       {
-        preset: 'conventionalcommits'
-      }
+        preset: "conventionalcommits",
+      },
     ],
-    '@semantic-release/release-notes-generator',
+    "@semantic-release/release-notes-generator",
     [
-      '@semantic-release/exec',
-      {
-        publishCmd: 'curl --header \"JOB-TOKEN: $CI_JOB_TOKEN\" --upload-file module.zip $PACKAGE_REGISTRY_URL/$VERSION/module.zip && curl --header \"JOB-TOKEN: $CI_JOB_TOKEN\" --upload-file module.json $PACKAGE_REGISTRY_URL/$VERSION/module.json'
-      }
-    ],
-    [
-      '@semantic-release/gitlab',
+      "@semantic-release/gitlab",
       {
         successComment: false,
         failComment: false,
         failTitle: false,
         assets: [
           {
-            url: `${process.env.PACKAGE_REGISTRY_URL}/${process.env.VERSION}/module.zip`,
-            label: 'module.zip',
-            type: 'package',
-            filepath: '/module.zip'
+            path: "module.zip",
+            label: "module.zip",
+            type: "package",
+            filepath: "/module.zip",
+            target: "generic_package",
           },
           {
-            url: `${process.env.PACKAGE_REGISTRY_URL}/${process.env.VERSION}/module.json`,
-            label: 'module.json',
-            filepath: '/module.json'
-          }
-        ]
-      }
+            path: "module.json",
+            label: "module.json",
+            filepath: "/module.json",
+            target: "generic_package",
+          },
+        ],
+      },
     ],
     [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
-        assets: [
-          'package.json',
-          'module.json'
-        ],
-        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}'
-      }
-    ]
-  ]
-}
+        assets: ["package.json", "module.json"],
+        message:
+          "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
